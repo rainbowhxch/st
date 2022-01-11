7fc3facb4105a302c383f5ebcf503bb6f48b48af
rainbowhxch <rainbowhxch@gmail.com>
2022-01-11 19:15:32 +0800

    add two patches and alter themes to colors folder

---
+++ colors/github-dimmed.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+    /* 8 normal colors */
+    [0] = "#22272e", /* black   */
+    [1] = "#ff938a", /* red     */
+    [2] = "#6bc46d", /* green   */
+    [3] = "#c69026", /* yellow  */
+    [4] = "#6cb6ff", /* blue    */
+    [5] = "#b083f0", /* magenta */
+    [6] = "#56d4dd", /* cyan    */
+    [7] = "#768390", /* white   */
+
+    /* 8 bright colors */
+    [8] = "#636e7b",  /* black   */
+    [9] = "#ff938a",  /* red     */
+    [10] = "#6bc46d", /* green   */
+    [11] = "#daaa3f", /* yellow  */
+    [12] = "#6cb6ff", /* blue    */
+    [13] = "#dcbdfb", /* magenta */
+    [14] = "#56d4dd", /* cyan    */
+    [15] = "#768390", /* white   */
+
+    [255] = 0,
+
+    /* more colors can be added after 255 to use with DefaultXX */
+    "#768390", /* cursor */
+    "#22272e", /* reverse cursor */
+    "#22272e", /* background */
+    "#768390", /* foreground */
+};
---
+++ colors/github-light.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+    /* 8 normal colors */
+    [0] = "#697179", /* black   */
+    [1] = "#d03d3d", /* red     */
+    [2] = "#14ce14", /* green   */
+    [3] = "#949800", /* yellow  */
+    [4] = "#0451a5", /* blue    */
+    [5] = "#bc05bc", /* magenta */
+    [6] = "#0598bc", /* cyan    */
+    [7] = "#586069", /* white   */
+
+    /* 8 bright colors */
+    [8] = "#666666",  /* black   */
+    [9] = "#cd3131",  /* red     */
+    [10] = "#14ce14", /* green   */
+    [11] = "#b5ba00", /* yellow  */
+    [12] = "#0451a5", /* blue    */
+    [13] = "#bc05bc", /* magenta */
+    [14] = "#0598bc", /* cyan    */
+    [15] = "#586069", /* white   */
+
+    [255] = 0,
+
+    /* more colors can be added after 255 to use with DefaultXX */
+    "#586069", /* cursor */
+    "#ffffff", /* reverse cursor */
+    "#ffffff", /* background */
+    "#586069", /* foreground */
+};
